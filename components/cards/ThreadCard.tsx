799f9dcd8d13245db3e7a9af5264dba06d6a3914
Adrian Rusan <rusan.adrian.ionut@gmail.com>
2023-08-10 10:02:18 +0300

    Implemented most of the features

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,6 +1,6 @@
-import { auth } from "@clerk/nextjs"
 import Link from "next/link";
 import Image from "next/image";
+import { formatDateString } from "@/lib/utils";
 
 
 interface Props {
@@ -102,7 +102,26 @@ const ThreadCard = ({
                         </div>
                     </div>
                 </div>
+                {/* TODO: Delete Thread*/}
+                {/* TODO: Show comment logos*/}
+                
             </div>
+                {!isComment && community && (
+                    <Link href={`/communities/${community.id}`} className="mt-5 flex items-center">
+                        <p className="text-subtle-medium text-gray-1">
+                            {formatDateString(createdAt)}
+                            {" "}- {community.name} Community
+                        </p>
+
+                        <Image 
+                            src={community.image}
+                            alt={community.name}
+                            width={14}
+                            height={14}
+                            className="mt-1 rounded-full object-cover"
+                        />
+                    </Link>
+                )}
         </article>
     )
 };
